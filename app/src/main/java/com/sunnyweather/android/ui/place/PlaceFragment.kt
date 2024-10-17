75d964e09566210d82d7d3f6f8610e6db7c021d7
Xjw <2430804627@qq.com>
2024-10-17 20:18:19 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
@@ -0,0 +1,112 @@
+package com.sunnyweather.android.ui.place
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.sunnyweather.android.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [PlaceFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class PlaceFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    val viewModel by lazy { ViewModelProvider(this).get(PlaceViewModel::class.java) }
+    private lateinit var adapter: PlaceAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var searchPlaceEdit: EditText
+    private lateinit var bgImageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val root = inflater.inflate(R.layout.fragment_place, container, false)
+
+        val layoutManager = LinearLayoutManager(activity)
+        recyclerView = root.findViewById(R.id.recyclerView)
+        searchPlaceEdit = root.findViewById(R.id.searchPlaceEdit)
+        bgImageView = root.findViewById(R.id.bgImageView)
+
+
+        adapter = PlaceAdapter(this, viewModel.placeList)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapter
+        searchPlaceEdit.addTextChangedListener { editable ->
+            val content = editable.toString().trim()
+            if (content.isNotEmpty()) {
+                viewModel.searchPlaces(content)
+            } else {
+                recyclerView.visibility = View.GONE
+                bgImageView.visibility = View.VISIBLE
+                viewModel.placeList.clear()
+                adapter.notifyDataSetChanged()
+            }
+        }
+
+        viewModel.placeLiveData.observe(viewLifecycleOwner) { result ->
+            val places = result.getOrNull()
+            if (places != null) {
+                recyclerView.visibility = View.VISIBLE
+                bgImageView.visibility = View.GONE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(places)
+                adapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        }
+
+        return root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment PlaceFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            PlaceFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
